97cecf572e74a7fecf3cc68982a9d22577b3b256
youzipi <blake_12@qq.com>
2016-02-01 23:17:22 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
-apt-get install docker
+apt-get update
+# apt-get dist-upgrade # 太慢 升级 所有包
 
-apt-get install mongodb
+
+apt-get git
+apt-get zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.zshrc ~/.zshrc.bak
+chsh -s /bin/zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+apt-get install -y docker
+
+apt-get install -y mongodb
 # uname -a
 # hostname
 # env 环境变量
@@ -15,11 +26,15 @@ apt-get install mongodb
 ###
 # aptitude
 
-apt-get install nginx
+apt-get install -y nginx
 
 wget  https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda2-2.4.1-Linux-x86_64.sh | sh
 
 ### python  
-apt-get install python-pip
+apt-get install -y python-pip
 pip install --upgrade pip
 
+pip install pyenv
+
+# reboot
+#sudo shutdown -r now
