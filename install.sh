5a6b7eda0cf206848f734d0386c2b34405c575f1
wuqiantai <wuqiantai@qianmi.com>
2016-02-15 10:05:02 +0800

    update

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@ apt-get update
 # apt-get dist-upgrade # 太慢 升级 所有包
 
 
-apt-get git
-apt-get zsh
+apt-get intall -y git zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.zshrc ~/.zshrc.bak
 chsh -s /bin/zsh
@@ -30,11 +29,20 @@ apt-get install -y nginx
 
 wget  https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda2-2.4.1-Linux-x86_64.sh | sh
 
-### python  
+### python
 apt-get install -y python-pip
 pip install --upgrade pip
 
-pip install pyenv
-
+# pyenv dependencies
+apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev
+# pyenv
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+# Anaconda
+conda update conda
+conda update anaconda
+# Scrapy
+conda install -y scrapy
+# seaborn
+conda install -y seaborn
 # reboot
-#sudo shutdown -r now
+reboot
---
+++ pyenv.md
@@ -0,0 +1,13 @@
+pyenv commands
+https://github.com/yyuu/pyenv/blob/master/COMMANDS.md
+```bash
+# pyenv
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+# Lists all available pyenv commands.
+pyenv commands
+# Lists all Python versions known to pyenv
+pyenv versions
+# List available remote versions of Python, including Anaconda, Jython, pypy, and stackless
+pyenv install -list
+# pyenv wiki = https://github.com/yyuu/pyenv/wiki
+```
